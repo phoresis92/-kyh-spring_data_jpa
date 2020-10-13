2b8b8257a6e3998a2f327b3e71baeb02f47b1950
kyd <husheart@naver.com>
2020-10-14 02:36:11 +0900

    3 - 예제 도메인 모델과 동작확인
    예제 도메인 모델

--- src/main/java/tk/youngdk/datajpa/domain/Member.java
+++ src/main/java/tk/youngdk/datajpa/domain/Member.java
@@ -2,27 +2,39 @@ package tk.youngdk.datajpa.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Getter @Setter
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString(of = {"id", "userName"})
+@ToString(of = {"id", "userName", "age"})
 public class Member {
     public Member(String userName) {
         this.userName = userName;
     }
 
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String userName;
 
-//    public void changeUserName(String userName) {
-//        this.userName = userName;
-//
-//    }
+    private int age;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
---
+++ src/main/java/tk/youngdk/datajpa/domain/Team.java
@@ -0,0 +1,32 @@
+package tk.youngdk.datajpa.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "teamName"})
+public class Team {
+
+    public Team(String teamName) {
+        this.teamName = teamName;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String teamName;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+}
