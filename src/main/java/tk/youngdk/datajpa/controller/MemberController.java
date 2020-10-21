c4687990ab83604794797ceb44a534b62a80e819
kyd <husheart@naver.com>
2020-10-21 23:55:45 +0900

    6 - Web 확장 도메인 클래스 컨버터
    확장 기능

---
+++ src/main/java/tk/youngdk/datajpa/controller/MemberController.java
@@ -0,0 +1,46 @@
+package tk.youngdk.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tk.youngdk.datajpa.domain.Member;
+import tk.youngdk.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        System.out.println("MemberController.findMember");
+
+        Member member = memberRepository.findById(id).get();
+        return member.getUserName();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        System.out.println("MemberController.findMember2");
+
+        /*
+        도메인 클래스 컨버터로 엔티티를 파라미터로 받으면,
+        이 엔티티는 단순 조회용으로만 사용해야한다.
+
+        트랜잭션이 없는 범위에서 엔티티를 조회했으므로,
+        엔티티를 변경해도 DB에 반영되지 않는다.
+        */
+
+        return member.getUserName();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
